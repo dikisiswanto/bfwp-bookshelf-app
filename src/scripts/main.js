aeeaa7707fd451dc5cdbd5d171d9a3e7026df0da
Diki Siswanto <dickyachmady@gmail.com>
2022-06-19 21:41:49 +0800

    fix wrong label title on markAsComplete button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -219,7 +219,7 @@ function pushBooksIntoShelfContainer(shelf, container) {
   shelfContainer.innerHTML = "";
   const filteredBooks = books.filter((book) => book.isComplete === (shelf === BOOKSHELF.READ));
   filteredBooks.forEach((book) => {
-    const textLabel = book.isComplete ? BOOKSHELF.READ.toLowerCase() : BOOKSHELF.UNREAD.toLocaleLowerCase();
+    const textLabel = book.isComplete ? BOOKSHELF.UNREAD.toLowerCase() : BOOKSHELF.READ.toLocaleLowerCase();
     const unReadIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"/></svg>';
     const readIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"/></svg>'
 
